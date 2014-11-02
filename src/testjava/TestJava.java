ff18d62fec7131525f0a135c651fd02bbbfcfc37
teacher <teacher@COMP101-01.itstep1.dp.ua>
2014-11-02 11:13:27 +0200

    v1

---
+++ src/testjava/TestJava.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testjava;
+
+/**
+ *
+ * @author teacher
+ */
+public class TestJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Slon ubeg na sever");
+        System.out.println("Vtoroy Slon toge ubeg na sever");
+    }
+}
