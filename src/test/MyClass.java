100724d2991479e20df3a5655fd81f5a48058cbc
teacher <teacher@COMP101-01.itstep1.dp.ua>
2014-11-02 11:19:02 +0200

    v2

---
+++ src/test/MyClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author teacher
+ */
+public class MyClass {
+    public void Test()
+    {
+        System.out.println("Kot ubeg na yug");
+    }
+}
