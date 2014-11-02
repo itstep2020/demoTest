a598ec3fd33c5ecff865358a9e35d4d73f50ca2b
teacher <teacher@COMP101-01.itstep1.dp.ua>
2014-11-02 12:54:05 +0200

    v5

--- src/test/MyClass.java
+++ src/test/MyClass.java
@@ -14,6 +14,6 @@ public class MyClass {
         System.out.println("Kot ubeg na yug");
         System.out.println("Lisa ubeg na yug");
         System.out.println("Kit ubeg na yug"); 
-        System.out.println("Los ubeg na yug"); 
+        System.out.println("Korovki ubeg na yug"); 
       }
 }
